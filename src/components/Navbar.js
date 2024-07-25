483829d4c1466b3f2f16e185a56298c18fe1be3d
Yash Baidya <yashkrbaidya@gmail.com>
2024-07-25 13:20:22 +0530

    ..

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,24 @@ import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import './Navbar.css';
+import logo from '../assets/images/logo.png';
 
 const CustomNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="/">Springdale Public School</Navbar.Brand>
+      <LinkContainer to="/">
+          <Navbar.Brand>
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Springdale Public School logo"
+            />
+            {' '}Springdale Public School
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
@@ -29,6 +41,9 @@ const CustomNavbar = () => {
             </LinkContainer>
             <LinkContainer to="/students">
               <Nav.Link>Students</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/gallery">
+              <Nav.Link>Gallery</Nav.Link>
             </LinkContainer>
             <Button href='/contact' variant="outline-light" className="ml-2">Contact Us</Button>
           </Nav>
